877cd234fee80219a0f70377d908f803d02aecc5
berziiii <brian@offpistedesigns.com>
2016-06-09 14:56:52 -0400

    Add attr_accessor to person.rb

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
 require 'pry'
+
+# Person class
+class Person
+  attr_accessor :favorite_food, :catchphrase
+
+  def initialize(given_name, surname)
+    @given_name = given_name
+    @surname = surname
+  end
+end
